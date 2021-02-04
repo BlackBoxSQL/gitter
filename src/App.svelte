c25bf1bde15defb94901d721d663dd1daed2d71e
BlackBoxSQL <pythonboy007@gmail.com>
2021-02-05 03:56:11 +0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from "svelte";
+    let users = [];
+    onMount(async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const json = await res.json();
+        users = json;
+    });
+</script>
+
+{#each users as user}
+    <h3>{user.title}</h3>
+    <br />
+    <p>{user.body}</p>
+{/each}
